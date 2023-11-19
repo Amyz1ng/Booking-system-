4c5c63d5ca3794d7f3dca8b592469fe59e4f4ffd
Amy Sawyer <amyleighsawyer97@gmail.com>
2023-11-19 23:29:27 +0000

    Added a print

--- .devcontainer/build-assets/db_setup.py
+++ .devcontainer/build-assets/db_setup.py
@@ -103,6 +103,7 @@ def insert_data():
     return jsonify({'message': 'Insert operation successful'})
 
 if __name__ == '__main__':
+    print(app.url_map)
     create_table()  # Create the table if it doesn't exist
     app.run(debug=True)
     app.run(port=5000)
